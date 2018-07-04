9799061d8d6da42768f9233d5b746385070f579f
unknown <hasush@gmail.com>
2018-07-04 11:32:40 -0400

    Finished face geneeration project.

--- deep-learning/face_generation/problem_unittests.py
+++ deep-learning/face_generation/problem_unittests.py
@@ -91,7 +91,6 @@ def test_discriminator(discriminator, tf_module):
         assert mock_variable_scope.call_args == mock.call('discriminator', reuse=True), \
             'tf.variable_scope called with wrong arguments in Discriminator Inference(reuse=True)'
 
-
 @test_safe
 def test_generator(generator, tf_module):
     with TmpMock(tf_module, 'variable_scope') as mock_variable_scope:
@@ -112,6 +111,27 @@ def test_generator(generator, tf_module):
             'tf.variable_scope not called in Generator Inference(reuse=True)'
         assert mock_variable_scope.call_args == mock.call('generator', reuse=True), \
             'tf.variable_scope called with wrong arguments in Generator Inference(reuse=True)'
+            
+# @test_safe
+# def test_generator(generator, tf_module):
+#     with TmpMock(tf_module, 'variable_scope') as mock_variable_scope:
+#         z = tf.placeholder(tf.float32, [None, 100])
+#         out_channel_dim = 5
+
+#         output = generator(z, out_channel_dim)
+#         _assert_tensor_shape(output, [None, 28, 28, out_channel_dim], 'Generator output (is_train=True)')
+#         assert mock_variable_scope.called, \
+#             'tf.variable_scope not called in Generator Training(reuse=false)'
+#         assert mock_variable_scope.call_args == mock.call('generator', reuse=False), \
+#             'tf.variable_scope called with wrong arguments in Generator Training(reuse=false)'
+
+#         mock_variable_scope.reset_mock()
+#         output = generator(z, out_channel_dim, False)
+#         _assert_tensor_shape(output, [None, 28, 28, out_channel_dim], 'Generator output (is_train=False)')
+#         assert mock_variable_scope.called, \
+#             'tf.variable_scope not called in Generator Inference(reuse=True)'
+#         assert mock_variable_scope.call_args == mock.call('generator', reuse=True), \
+#             'tf.variable_scope called with wrong arguments in Generator Inference(reuse=True)'
 
 
 @test_safe
